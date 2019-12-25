1aed8cd3a5d7410439e7e8a15867395f9019ae0f
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-25 13:37:34 +0200

    CreateUser mutation, using default values

--- schema.py
+++ schema.py
@@ -1,11 +1,12 @@
 import graphene
 import json
+import uuid
 from datetime import datetime
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 
 class Query(graphene.ObjectType):
@@ -26,16 +27,33 @@ class Query(graphene.ObjectType):
     def resolve_is_admin(self, info):
         return True
 
+class CreateUser(graphene.Mutation):
 
-schema = graphene.Schema(query=Query)
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        user = User(username=username)
+
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 result = schema.execute(
     '''
-    {
-        users(limit: 2) {
-            id
-            username
-            createdAt
+    mutation {
+        createUser(username: "Jeff") {
+            user {
+                id
+                username
+                createdAt
+            }
         }
     }
     '''
