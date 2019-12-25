fccbd0caeace63ab85d99b634c57555a1a7aa69b
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-25 12:28:04 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+```bash
+docker-compose build
+docker-compose run graphql /bin/bash
+docker-compose run node /bin/bash
+```
---
+++ schema.py
@@ -0,0 +1,19 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, info):
+        return "world"
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+    {
+        hello
+    }
+    '''
+)
+
+print(result.data.items())
