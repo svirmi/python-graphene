e698660d5fc67dc07efcc3fee24c74126f4897e8
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-12-25 13:08:42 +0200

    dealing with more complex data

--- schema.py
+++ schema.py
@@ -1,9 +1,24 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
+
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User)
     hello = graphene.String()
     is_admin = graphene.Boolean()
+
+    def resolve_users(self, info):
+        return [
+            User(id='1', username="John", created_at=datetime.now()),
+            User(id='2', username="Bill", created_at=datetime.now()),
+            User(id='3', username="Donald", created_at=datetime.now()),
+        ]
 
     def resolve_hello(self, info):
         return "world"
@@ -11,12 +26,17 @@ class Query(graphene.ObjectType):
     def resolve_is_admin(self, info):
         return True
 
-schema = graphene.Schema(query=Query, auto_camelcase=False)
+
+schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     '''
     {
-        is_admin
+        users {
+            id
+            username
+            createdAt
+        }
     }
     '''
 )
