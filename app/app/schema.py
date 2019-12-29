5a9b4ccf814727a4fe2bb1fdc98938f21d58c135
MS <mixa.ru.ru@gmail.com>
2019-12-29 11:52:42 +0200

    mutation code

--- app/app/schema.py
+++ app/app/schema.py
@@ -5,4 +5,7 @@ import tracks.schema
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
