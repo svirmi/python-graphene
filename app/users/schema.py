b2c18f12454d3f5b894a2e9ce029965811b7bfac
MS <mixa.ru.ru@gmail.com>
2019-12-29 12:02:18 +0200

    users schema

---
+++ app/users/schema.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String()
+        password = graphene.String()
+        email = graphene.String()
+
